05e5e4b2ad73f6e8e80a6364db8c1dc2af29fa9d
abhidubey007 <abhidb95@gmail.com>
2022-09-27 19:53:50 +0530

    Add Product Api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const cors = require("cors")
 require('./db/config');
-const User = require('./db/Users');
+
+const User = require('./db/User');
+const Product = require('./db/Product')
 const app = express();
 
 app.use(express.json())
@@ -9,8 +11,28 @@ app.use(cors())
 
 app.post("/register", async (req, resp) => {
     let user = new User(req.body);
-    let result = await user.save()
-    resp.send(req.body)
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password;
+    resp.send(result)
+})
+
+app.post("/login", async (req, resp) => {
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select("-password")
+        if (user) {
+            resp.send(user)
+        } else {
+            resp.send({ result: "No User Found" })
+        }
+    }
+
+})
+
+app.post("/add-product", async (req, resp) => {
+    let product = new Product(req.body);
+    let result = await product.save();
+    resp.send(result)
 })
 
 app.listen(5000)
