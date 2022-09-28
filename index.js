947e435c68e03549c9d48bfd90c37e06d12280b3
abhidubey007 <abhidb95@gmail.com>
2022-09-28 17:01:37 +0530

    Product api

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.post("/register", async (req, resp) => {
 
 app.post("/login", async (req, resp) => {
     if (req.body.password && req.body.email) {
-        let user = await User.findOne(req.body).select("-password")
+        const user = await User.findOne(req.body).select("-password")
         if (user) {
             resp.send(user)
         } else {
@@ -32,6 +32,40 @@ app.post("/login", async (req, resp) => {
 app.post("/add-product", async (req, resp) => {
     let product = new Product(req.body);
     let result = await product.save();
+    resp.send(result)
+})
+
+app.get("/products", async (req, resp) => {
+    const products = await Product.find()
+    if (products.length > 0) {
+        resp.send(products)
+    } else {
+        resp.send({ result: "No Products Found" })
+    }
+})
+
+app.delete("/product/:id", async (req, resp) => {
+    const result = await Product.deleteOne({ _id: req.params.id })
+    resp.send(result)
+})
+
+app.get("/product/:id", async (req, resp) => {
+    const result = await Product.findOne({ _id: req.params.id })
+    if (result) {
+        resp.send(result)
+    } else {
+        resp.send({ result: "No Product Found" })
+    }
+})
+
+app.put("/product/:id", async (req, resp) => {
+    const result = await Product.updateOne(
+        {
+            _id: req.params.id
+        },
+        {
+            $set: req.body
+        })
     resp.send(result)
 })
 
